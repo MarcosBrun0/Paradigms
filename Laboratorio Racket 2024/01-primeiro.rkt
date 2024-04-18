cf90a5bedc89e31f0af2b9b69175372b54348c6a
MarcosBrun0 <mbrunocampos20@gmail.com>
2024-04-18 19:01:48 -0300

    racket-init

---
+++ Laboratorio Racket 2024/01-primeiro.rkt
@@ -0,0 +1,23 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname 01-primeiro) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+;; Introdução à Linguagem Racket (Scheme)
+;; Prof. Ausberto S. Castro Vera       (ascv@uenf.br)
+;; UENF-CCT-LCMAT - Curso de Ciencia da Computacao
+;; 2024
+;; Aluno: Marcos Bruno P. Campos      <===========  escreva seu nome aqui
+;; 
+;; Liguagem Advanced Student       ;; define a linguagem default
+;; O primeiro programa Racket
+;; ---------------------------------------------------
+(begin
+  (newline)
+  (display "Bom dia, UENF 30 anos, Nota 5 MEC 2024")
+  (newline)
+  (display "Bemvindo a Linguagem Racket-Scheme! ") ;<------ mostrar na tela
+  (newline)
+  (display "Sou o aluno Marcos Bruno P. Campos")
+  (newline)
+  (display "Pratica 01-linguagem Racket, Marcos Bruno P. Campos, 18/04/24")
+ )
+
