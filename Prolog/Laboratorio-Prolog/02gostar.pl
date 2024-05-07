684efa4d0e99d2081438c8ea6df3f72bd2d5978d
marcosbrun0 <Mbrunocampos20@gmail.com>
2024-05-07 08:28:00 -0300

    scilab

---
+++ Prolog/Laboratorio-Prolog/02gostar.pl
@@ -0,0 +1,16 @@
+% Arquivo:      02gostar.pl
+% Prof. Ausberto S. Castro Vera
+% Disciplina : Paradigmas de Ling. de Programacao
+% UENF-CCT-LCMAT-CC
+% Ano: 2024
+% Aluno: Fulano  da Silva
+%-------------------------------------------------
+
+gostade(maria,suco).
+gostade(maria,vinho).
+gostade(maria,gatos).
+gostade(pedro,vinho).
+gostade(pedro,maria).
+gostade(pedro,pizza).
+gostade(pedro,flores).
+
